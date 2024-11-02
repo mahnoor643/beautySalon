13dcb07f306063a95f0c5fae3f425b6de6503c82
mahnoor643 <mahnoort643@gmail.com>
2024-11-03 01:47:05 +0500

    db and admin template

---
+++ assets/conn.php
@@ -0,0 +1,12 @@
+<?php
+$server="localhost";
+$user="root";
+$pwd="";
+$db="beautySalon";
+$conn = mysqli_connect($server, $user, $pwd, $db);
+// if($conn){
+//     echo "Connected successfully";
+// }else{
+//     echo "Connection failed: ". mysqli_connect_error();
+// }
+?>
