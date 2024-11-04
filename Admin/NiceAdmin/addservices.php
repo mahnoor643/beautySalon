1e8f4106f0ac1206db6f8fa04a8e28c8bbd3a673
mahnoor643 <mahnoort643@gmail.com>
2024-11-05 02:27:21 +0500

    admin modified

---
+++ Admin/NiceAdmin/addservices.php
@@ -0,0 +1,74 @@
+<?php 
+require "./assets/header.php";
+require "../../assets/conn.php";
+
+// Add Service
+if(isset($_POST['submit'])) {
+    $title = $_POST['title'];
+    $desc = $_POST['desc'];
+    $sql = "INSERT INTO services (serviceTitle, serviceDescription) VALUES ('$title', '$desc')";
+    $query=mysqli_query($conn, $sql);
+    if($query) {
+        echo "<script>alert('Service Added Successfully');</script>";
+    } else {
+        echo "Error: ". $sql. "<br>". mysqli_error($conn);
+    }
+}
+?>
+
+<main id="main" class="main">
+
+    <div class="pagetitle">
+        <h1>Service Add</h1>
+        <!-- <nav>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+      <li class="breadcrumb-item">Forms</li>
+      <li class="breadcrumb-item active">Layouts</li>
+    </ol>
+  </nav> -->
+    </div><!-- End Page Title -->
+    <section class="section">
+        <div class="row">
+            <div class="col-lg-12">
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Add Service</h5>
+
+                        <!-- Horizontal Form -->
+                        <form method="post">
+                            <div class="row mb-3">
+                                <label for="inputEmail3" class="col-sm-2 col-form-label">Title</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="title" class="form-control" id="inputText">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label for="inputEmail3" class="col-sm-2 col-form-label">Description</label>
+                                <div class="col-sm-10">
+                                    <textarea rows="6" name="desc" class="form-control"></textarea>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-sm-2"></div>
+                                <div class="col-sm-10">
+                                    <button class="btn-warning btn" name="submit" type="submit">Add</button>
+                                </div>
+                            </div>
+                        </form><!-- End Horizontal Form -->
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+</main><!-- End #main -->
+
+
+
+
+<?php require "./assets/footer.php"; ?>
---
+++ Admin/NiceAdmin/services.php
@@ -0,0 +1,72 @@
+<?php 
+require "./assets/header.php";
+require "../../assets/conn.php";
+$sql="SELECT * FROM services";
+$sqlRun=mysqli_query($conn,$sql);
+
+?>
+
+<main id="main" class="main">
+
+<div class="pagetitle">
+  <h1>Services</h1>
+  <!-- <nav>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+      <li class="breadcrumb-item">Tables</li>
+      <li class="breadcrumb-item active">General</li>
+    </ol>
+  </nav> -->
+</div><!-- End Page Title -->
+
+<section class="section">
+  <div class="row">
+
+    <div class="col-lg-12">
+
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Services</h5>
+
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">ID</th>
+                <th scope="col">Description</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+                <?php
+                $i=1;
+                 while($row=mysqli_fetch_assoc($sqlRun)) {
+                    ?>
+                  <tr>
+                    <th scope="row"><?php echo $i;?></th>
+                    <td><?php echo $row['serviceTitle'];?></td>
+                    <th><?php echo $row['serviceID'];?></th>
+                    <td><?php echo $row['serviceDescription'];?></td>
+                    <td><a href="serviceExpertise.php?id=<?php echo $row['serviceID']; ?>">
+                                            <?php echo $row['l_name']; ?>
+                                        </a></td>
+                  </tr>
+                <?php 
+                $i++;    
+            }
+                ?>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+     
+
+    </div>
+  </div>
+</section>
+
+</main><!-- End #main -->
+
+<?php require "./assets/footer.php"; ?>
